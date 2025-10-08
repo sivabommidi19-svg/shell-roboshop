34c5f5c18bf5a05f7adc351d6ecc3ed0d58a4170
sivabommidi19-svg <sivabommidi19@gmail.com>
2025-10-08 23:46:38 +0530

    session-18

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -14,10 +14,11 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.daws86b.fun
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_Name.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_Name.log"  #/var/log/shell-scipt/16log.log
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: please run this script with root privelege"
     exit 1 # failure is other than 0
@@ -35,20 +36,20 @@ if [ $? -ne 0 ]; then
 else
     echo -e "user already exit ...$Y SKIPPING $N"
 fi
-
+echo "Installing nodejs 20 ..$G SUCCESS $N"
 mkdir -p /app 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
 cd /app 
 rm -rf /app/*
 unzip /tmp/catalogue.zip &>>$LOG_FILE
-
 npm install &>>$LOG_FILE
 cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue &>>$LOG_FILE
+echo -e "catalogue application setup... $G SUCCESS $N" 
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongoshdbjd -y &>>$LOG_FILE
+dnf install mongodb-mongoshd -y &>>$LOG_FILE
 
 INDEX=$(mongosh mongodb.daws86b.fun --quiet --eval "db.getMongo().getDBNames().indexOf('admin')")
 if [ $INDEX -le 0 ]; then
@@ -58,5 +59,6 @@ else
 fi
 
 systemctl restart catalogue
+echo -e "Loading products and restarting catalogue ... $G SUCCESS $N"
 
 
