90fd2fc8dd0a021b8dfa40a09bba95842362800c
sivabommidi19-svg <sivabommidi19@gmail.com>
2025-10-08 19:56:01 +0530

    session-18

--- mongodb.sh
+++ mongodb.sh
@@ -37,3 +37,10 @@ VALIDATE $? "Enable MongoDB"
 
 systemctl start mongod 
 VALIDATE $? "Start MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connectionts to MongoDB"
+
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
